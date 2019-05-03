183f6ea7f6a3d67d699c8dfb391c85f166a5c554
Дарья Жоголь <da18121996@yandex.ru>
2019-05-03 13:52:38 +0300

    Player output and class array creation

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-
-var N_SIZE = 5;
-
+var N_SIZE = 5,
+player ='X';
+/**
+ * Show table tic-tac
+ */
 function showTable(){
-    boxes[N_SIZE,N_SIZE];
+    var boxes = [[N_SIZE],[N_SIZE]];
     var table = document.createElement('table');
     table.setAttribute('border',1);
     table.setAttribute('cellspacing',0);
     for(var i = 0; i < N_SIZE ; i++){
+        if(!boxes[i])
+            boxes[i]=[];
         var row = document.createElement('tr');
         table.appendChild(row);
-        for(var j=0; j<N_SIZE;j++){
+        for(var j = 0; j<N_SIZE;j++){
             var cell = document.createElement('td');
-            row.appendChild(cell);
-        }
+            cell.classList.add( i,j);
+            cell.addEventListener('click',record);
+            row.appendChild(cell);            
+            boxes[i][j]=1;
+           // console.log(boxes[i][j]);
+        }        
     }
     document.getElementById("tictactoe").appendChild(table);
 }
 /*
 * New game
 */
-
 function NewGame(){
- N_SIZE =  prompt('Size', 100); 
+ N_SIZE =  prompt('Size', 5); 
  showTable();
 } 
+/*
+ * Write player
+ */
+function record(){
+player = player ==='X'?'O':'X';
+document.getElementById("player").textContent='Player '+player;
+}
+NewGame();
 
- NewGame();
