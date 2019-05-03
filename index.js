4faa723d72e7976262b268d0ead57e5a837a98f0
Дарья Жоголь <da18121996@yandex.ru>
2019-05-03 17:20:10 +0300

    Added win check and write to array of values

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var N_SIZE = 5,
-player ='X';
+player ='X',
+count=0;
+boxes=new Array();
 /**
  * Show table tic-tac
  */
 function showTable(){
-    var boxes = [[N_SIZE],[N_SIZE]];
+    boxes = [[N_SIZE],[N_SIZE]];
     var table = document.createElement('table');
     table.setAttribute('border',1);
     table.setAttribute('cellspacing',0);
     for(var i = 0; i < N_SIZE ; i++){
-        if(!boxes[i])
             boxes[i]=[];
         var row = document.createElement('tr');
         table.appendChild(row);
@@ -19,7 +20,6 @@ function showTable(){
             cell.addEventListener('click',record);
             row.appendChild(cell);            
             boxes[i][j]=1;
-           // console.log(boxes[i][j]);
         }        
     }
     document.getElementById("tictactoe").appendChild(table);
@@ -28,15 +28,74 @@ function showTable(){
 * New game
 */
 function NewGame(){
- N_SIZE =  prompt('Size', 5); 
+ N_SIZE =  prompt('Size', 3); 
  showTable();
 } 
 /*
  * Write player
  */
 function record(){
-player = player ==='X'?'O':'X';
-document.getElementById("player").textContent='Player '+player;
+    cell = event.target;     
+    arr = cell.className.split(" ");
+    readingClassList(arr);    
+}
+
+/*
+* record turn and name class in array
+ */
+function readingClassList(arr){
+    i = +arr[0];
+    j = +arr[1];
+    if(arr[1]===undefined) 
+        j=i;
+    turn(i,j);
+} 
+
+function turn(i,j){
+    if(checkCell(i,j)) {
+        count++;
+        boxes[i][j]=player ==='X'?'X':'O'; 
+        event.target.style.background= player ==='X'?'green':'red';
+        event.target.textContent= player ==='X'?'X':'O'; 
+        if(checkWin(player)){
+            alert('Win '+player);
+            window.location.reload();
+        }
+        if(count==N_SIZE*N_SIZE) {
+            alert('New Game');  
+            window.location.reload();
+        }
+        player = boxes[i][j] ==='X'?'O':'X'; 
+        document.getElementById("player").textContent='Player '+player;      
+    } 
+    console.log(boxes[i][j]);     
+}
+/**
+ * Win check
+ */
+function checkWin(a){  
+    countG=0; 
+    for(var i = 0; i < N_SIZE ; i++){
+        countV=0;
+        countH=0;
+        if(boxes[i][i]===a)countG++;
+        for(var j = 0; j<N_SIZE;j++){
+            if (boxes[i][j]===a) countV++;
+            if (boxes[j][i]===a) countH++;
+        }
+        if(countH==N_SIZE||countV==N_SIZE)return true;
+    }
+   if(countG==N_SIZE) return true; 
+    return false; 
+}
+
+/**
+ * Empty cell check
+ */
+function  checkCell(i,j){
+    if(boxes[i][j]===1)return true;
+    else return false;
 }
 NewGame();
+
 
