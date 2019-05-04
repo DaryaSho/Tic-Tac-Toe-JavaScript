dd26037ed3f98fcec6ba780c97b158d996352555
Дарья Жоголь <da18121996@yandex.ru>
2019-05-04 15:53:19 +0300

    Formatting document and adding "use strict"

--- index.js
+++ index.js
@@ -1,103 +1,87 @@
-var N_SIZE = 5,
-player ='X',
-count=0;
-boxes=new Array();
-/**
- * Show table tic-tac
- */
-function showTable(){
-    boxes = [[N_SIZE],[N_SIZE]];
+"use strict";
+var size = 5,
+    player = 'X',
+    boxes = [],
+    count = { countH: 0, countG: 0, countG1: 0, countG: 0, all: 0 };
+
+newGame();
+
+function newGame() {
+    size = prompt('Size', 3);
+    showTable();
+}
+
+function showTable() {
+    boxes = [
+        [size],
+        [size]
+    ];
     var table = document.createElement('table');
-    table.setAttribute('border',1);
-    table.setAttribute('cellspacing',0);
-    for(var i = 0; i < N_SIZE ; i++){
-            boxes[i]=[];
+    table.setAttribute('border', 1);
+    table.setAttribute('cellspacing', 0);
+    for (var i = 0; i < size; i++) {
+        boxes[i] = [];
         var row = document.createElement('tr');
         table.appendChild(row);
-        for(var j = 0; j<N_SIZE;j++){
+        for (var j = 0; j < size; j++) {
             var cell = document.createElement('td');
-            cell.classList.add( i,j);
-            cell.addEventListener('click',record);
-            row.appendChild(cell);            
-            boxes[i][j]=1;
-        }        
+            cell.classList.add(i, j);
+            cell.addEventListener('click', record);
+            row.appendChild(cell);
+            boxes[i][j] = null;
+        }
     }
     document.getElementById("tictactoe").appendChild(table);
 }
-/*
-* New game
-*/
-function NewGame(){
- N_SIZE =  prompt('Size', 3); 
- showTable();
-} 
-/*
- * Write player
- */
-function record(){
-    cell = event.target;     
-    arr = cell.className.split(" ");
-    readingClassList(arr);    
+
+
+function record() {
+    var cell = event.target;
+    var arr = cell.className.split(" ");
+    readingClassList(arr);
 }
 
-/*
-* record turn and name class in array
- */
-function readingClassList(arr){
-    i = +arr[0];
-    j = +arr[1];
-    if(arr[1]===undefined) 
-        j=i;
-    turn(i,j);
-} 
-
-function turn(i,j){
-    if(checkCell(i,j)) {
-        count++;
-        boxes[i][j]=player ==='X'?'X':'O'; 
-        event.target.style.background= player ==='X'?'green':'red';
-        event.target.textContent= player ==='X'?'X':'O'; 
-        if(checkWin(player)){
-            alert('Win '+player);
-            window.location.reload();
-        }
-        else if(count==N_SIZE*N_SIZE) {
-            alert('New Game');  
-            window.location.reload();
-        }
-        player = boxes[i][j] ==='X'?'O':'X'; 
-        document.getElementById("player").textContent='Player '+player;      
-    } 
-    console.log(boxes[i][j]);     
+function readingClassList(arr) {
+    var i = +arr[0];
+    var j = +arr[1];
+    if (arr[1] === undefined)
+        j = i;
+    turn(i, j);
 }
-/**
- * Win check
- */
-function checkWin(a){  
-    countG=0; 
-    countG1=0;
-    for(var i = 0; i < N_SIZE ; i++){
-        countV=0;
-        countH=0;
-        if(boxes[i][i]===a)countG++;
-        if(boxes[i][N_SIZE-1-i]===a)countG1++;
-        for(var j = 0; j<N_SIZE;j++){
-            if (boxes[i][j]===a) countV++;
-            if (boxes[j][i]===a) countH++;
-        }       
-        if(countH==N_SIZE||countV==N_SIZE)return true;
+
+function turn(i, j) {
+    if (boxes[i][j] === null) {
+        count.all++;
+        boxes[i][j] = player === 'X' ? 'X' : 'O';
+        event.target.style.background = player === 'X' ? 'green' : 'red';
+        event.target.textContent = player === 'X' ? 'X' : 'O';
+        if (checkWin(player)) {
+            alert('Win ' + player);
+            window.location.reload();
+        } else if (count.all == size * size) {
+            alert('New Game');
+            window.location.reload();
+        }
+        player = boxes[i][j] === 'X' ? 'O' : 'X';
+        document.getElementById("player").textContent = 'Player ' + player;
     }
-   if(countG==N_SIZE||countG1==N_SIZE) return true; 
-    return false; 
+    console.log(boxes[i][j]);
 }
 
-/**
- * Empty cell check
- */
-function  checkCell(i,j){
-    if(boxes[i][j]===1)return true;
-    else return false;
+function checkWin(player) {
+    count.countG = 0;
+    count.countG1 = 0;
+    for (var i = 0; i < size; i++) {
+        count.countH = 0;
+        count.countV = 0;
+        if (boxes[i][i] === player) count.countG++;
+        if (boxes[i][size - 1 - i] === player) count.countG1++;
+        for (var j = 0; j < size; j++) {
+            if (boxes[i][j] === player) count.countV++;
+            if (boxes[j][i] === player) count.countH++;
+        }
+        if (count.countH == size || count.countV == size) return true;
+    }
+    if (count.countG == size || count.countG1 == size) return true;
+    return false;
 }
-NewGame();
-
-
